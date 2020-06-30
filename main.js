d0f78d698ef4e6b93230fe7d0a458c22bbe6018f
emanuele5180 <macina5180>
2020-06-30 15:29:31 +0200

    numeri generati dal pc

---
+++ main.js
@@ -0,0 +1,24 @@
+// Descrizione
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
+
+//il computer deve generare 16 numeri casuali tra 1 e 100 quindi deve ciclare per 16 volte pescando un numero a caso e pushandolo in un array
+
+
+var numeriGenerati = numero;
+
+for (var i = 0; i < 16; i++) {
+  var numero = parseInt(Math.floor ( Math.random()*100)+1);
+  console.log(numero);
+}
